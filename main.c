168ecf0a34274727ae8ab3ba4b05ce5d8021eaf3
justemax <max.hofferlormand@hotmail.fr>
2020-12-23 13:28:28 +0100

    Ajout de plus de para mais tjrs moins efficasse

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
-#define TAILLE_TAB 1000000
+#define TAILLE_TAB 21474836
 #define MIN_ALEA 1
 #define MAX_ALEA 10000
 
@@ -15,7 +15,7 @@ void triFusion(int* tab, int debut, int fin);
 
 
 int nb_aleatoire(int min, int max) {
-    return rand()%(max-min+1) + min;
+	return rand()%(max-min+1) + min;
 }
 
 void init_tab_alea(int* tab){
@@ -33,61 +33,64 @@ void afficher_tab(int* tab){
 }
 
 void fusion(int* tab, int debut, int milieu, int fin){
-    int n1 = milieu - debut + 1;
-    int n2 = fin - milieu;
- 
-    int G[n1], D[n2];
- 
-    for (int i = 0; i < n1; i++)
-        G[i] = tab[debut + i];
-    for (int j = 0; j < n2; j++)
-        D[j] = tab[milieu + 1 + j];
- 
-    // maintient trois pointeurs, un pour chacun des deux tableaux et un pour
-    // maintenir l'index actuel du tableau trié final
-    int i, j, k;
-    i = 0;
-    j = 0;
-    k = debut;
- 
-    while (i < n1 && j < n2){
-        if (G[i] <= D[j]){
-            tab[k] = G[i];
-            i++;
-        }else{
-            tab[k] = D[j];
-            j++;
-        }
-        k++;
-    }
- 
-    // Copiez tous les éléments restants du tableau non vide
-    while (i < n1) {
-        tab[k] = G[i];
-        i++;
-        k++;
-    }
- 
-    while (j < n2) {
-        tab[k] = D[j];
-        j++;
-        k++;
-    }
+	int n1 = milieu - debut + 1;
+	int n2 = fin - milieu;
+	
+	int* G = NULL;
+	int* D = NULL;
+	//int G[n1], D[n2];
+	G = malloc(n1*sizeof(int));
+	D = malloc(n2*sizeof(int));
+	for (int i = 0; i < n1; i++)
+		G[i] = tab[debut + i];
+	for (int j = 0; j < n2; j++)
+		D[j] = tab[milieu + 1 + j];
+
+	// maintient trois pointeurs, un pour chacun des deux tableaux et un pour
+	// maintenir l'index actuel du tableau trié final
+	int i, j, k;
+	i = 0;
+	j = 0;
+	k = debut;
+
+	while (i < n1 && j < n2){
+		if (G[i] <= D[j]){
+			tab[k] = G[i];
+			i++;
+		}else{
+			tab[k] = D[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copiez tous les éléments restants du tableau non vide
+	while (i < n1) {
+		tab[k] = G[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		tab[k] = D[j];
+		j++;
+		k++;
+	}
 }
-     
+
 // Tri par fusion
 void triFusion(int* tab, int debut, int fin) {
-    if (debut < fin){
- 
-        // Trouvez le point milieu pour diviser le tableau en deux moitiés
-        int milieu = (debut + fin) / 2;
- 
-        triFusion(tab, debut, milieu);
-        triFusion(tab, milieu+1, fin);
- 
-        // Fusionnez les deux moitiés triées
-        fusion(tab, debut, milieu, fin);
-    }
+	if (debut < fin){
+
+		// Trouvez le point milieu pour diviser le tableau en deux moitiés
+		int milieu = (debut + fin) / 2;
+
+		triFusion(tab, debut, milieu);
+		triFusion(tab, milieu+1, fin);
+
+		// Fusionnez les deux moitiés triées
+		fusion(tab, debut, milieu, fin);
+	}
 }
 
 int main(void) {
@@ -97,10 +100,10 @@ int main(void) {
 	srand((unsigned)time(NULL)); // A faire 1 fois, pour les fonctions aléatoire
 
 	init_tab_alea(p_tab);
-//	afficher_tab(p_tab);
+	//	afficher_tab(p_tab);
 
 	triFusion(p_tab, 0, TAILLE_TAB);
-//	afficher_tab(p_tab);
+	//	afficher_tab(p_tab);
 
 	free(p_tab);
 	double stop = omp_get_wtime();
